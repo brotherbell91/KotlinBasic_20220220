b8c4943f12a1309e08b18253661aa8e8b56d0b6e
이형종 <hj36879@naver.com>
2022-02-20 14:19:08 +0900

    var / val 만들고 , 대입해보기  - val은 한번만 대입 가능

--- app/src/main/java/com/hyeongjong/kotlinbasic_20220220/MainActivity.kt
+++ app/src/main/java/com/hyeongjong/kotlinbasic_20220220/MainActivity.kt
@@ -34,7 +34,29 @@ class MainActivity : AppCompatActivity() {
         btnToast.setOnClickListener {
 
 //            토스트를 띄워보자.
+//            toast라고 적고 Create a new toast 선택
             Toast.makeText(this,"토스트 버튼 눌림",Toast.LENGTH_SHORT).show()
+        }
+
+        btnVariablePractice.setOnClickListener {
+
+//            var/val 에 대한 연습 / 체험
+
+            var myName : String //내용 변경 가능
+
+            val friendName : String //한번 대입한 값으로 고정
+
+//            myName - 내 이름, friendName - 친구 이름
+
+            myName = "이형종"
+
+            friendName = "조경진"
+
+//            내이름 / 친구이름 다시 대입 (데이터 변경)
+
+            myName = "이종형" // 추가로 다시 대입해도 됨. var
+//            friendName = "조상민" //한번 넣은 값 계속 사용. val
+
         }
     }
 }
